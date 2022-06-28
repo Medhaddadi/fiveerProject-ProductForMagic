9e1fec5617489491209cf8824dc6bf11002fc035
Medhaddadi <77499801+Medhaddadi@users.noreply.github.com>
2022-06-28 21:31:27 +0100

    v 1.4

--- js/myjs.js
+++ js/myjs.js
@@ -1,1 +1,30 @@
-let t=txt.style
+const box = document.getElementById('box');
+
+const btn = document.getElementById('btn');
+
+btn.addEventListener('click', function handleClick() {
+  if (box.style.display === 'none') {
+    box.style.display = 'block';
+
+    btn.textContent = 'Hide div';
+  } else {
+    box.style.display = 'none';
+
+    btn.textContent = 'Show div';
+  }
+});
+const box2 = document.getElementById('box2');
+
+const btn2 = document.getElementById('btn2');
+
+btn2.addEventListener('click', function handleClick2() {
+  if (box2.style.display === 'none') {
+    box2.style.display = 'block';
+
+    btn2.textContent = 'Hide div';
+  } else {
+    box2.style.display = 'none';
+
+    btn2.textContent = 'Show div';
+  }
+});
